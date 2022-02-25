36744c2ea3dc09711ad1ec35b147ee59bdfd37dc
Iman Daneshi <iman_daneshi@yahoo.com>
2022-02-25 10:34:24 +0330

    Extracting and Reading Data

--- sitka_highs.py
+++ sitka_highs.py
@@ -4,4 +4,14 @@ filename ='data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print (header_row)
+
+    #for index , culomn_header in enumerate(header_row):
+    #    print (index,culomn_header)
+
+    #get high tempreture from the the file
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)   
+
+    print (highs)
